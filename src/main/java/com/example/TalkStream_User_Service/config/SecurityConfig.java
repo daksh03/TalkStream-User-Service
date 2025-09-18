1954fa554ed75f6173a1fdb33ee6934065723f2e
daksh03 <dakshchoudhary.1234@gmail.com>
2025-09-18 12:07:32 +0530

    Basic Security for Authentication

---
+++ src/main/java/com/example/TalkStream_User_Service/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.TalkStream_User_Service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+
+	 @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	            .csrf(AbstractHttpConfigurer::disable)   // ✅ new style
+	            .authorizeHttpRequests(auth -> auth
+	                .requestMatchers("/addUser").permitAll()
+	                .anyRequest().authenticated()
+	            );
+	        return http.build();
+	 }
+}
